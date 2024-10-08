02fc1d9c113e50baf5381d9c920a31525212ac4e
Floris999 <floris__2@hotmail.com>
2024-10-08 08:16:43 +0200

    Update email_verzinding.php

--- includes/email_verzending.php
+++ includes/email_verzending.php
@@ -1,48 +1,50 @@
 <?php
-// Functie voor het verzenden van de e-mail met urenoverzicht
-function urenregistratie_verzend_email($post_id) {
-    if (get_post_type($post_id) !== 'uren') {
+
+function send_hours_submission_email($post_id)
+{
+    if (get_post_type($post_id) != 'uren') {
         return;
     }
 
-    // Haal het e-mailadres op uit de instellingen
-    $notification_email = get_option('urenregistratie_notification_email');
-    if (!$notification_email) {
-        // Gebruik het admin e-mailadres als fallback
-        $notification_email = get_option('admin_email');
-    }
-
-    $uren_tekst = get_post_field('post_content', $post_id);
-
-    // Haal de gebruiker op die de uren heeft ingediend
-    $user_id = get_post_field('post_author', $post_id);
+    $user_id = get_post_meta($post_id, 'user_id', true);
     $user_info = get_userdata($user_id);
     $user_name = $user_info ? $user_info->display_name : 'Onbekende gebruiker';
+    $user_email = $user_info ? $user_info->user_email : '';
 
-    // Haal het weeknummer op uit de postmeta
+    $opdrachtgever_id = get_user_meta($user_id, 'opdrachtgever_id', true);
+    $opdrachtgever_info = get_userdata($opdrachtgever_id);
+
+    $admin_email = get_option('urenregistratie_notification_email', '');
+
+    if (!$opdrachtgever_info || !$admin_email) {
+        return;
+    }
+
     $weeknummer = get_post_meta($post_id, 'weeknummer', true);
+    $uren_tekst = get_post_meta($post_id, 'uren', true);
 
-    // Uren omzetten naar een leesbaar formaat en totaal berekenen
-    $uren = json_decode($uren_tekst, true);
     $uren_leesbaar = '';
     $totaal_uren = 0;
-    foreach ($uren as $dag => $uren_per_dag) {
+    foreach ($uren_tekst as $dag => $uren_per_dag) {
         if (!empty($uren_per_dag)) {
             $uren_leesbaar .= ucfirst($dag) . ': ' . $uren_per_dag . ' uur<br>';
             $totaal_uren += (int)$uren_per_dag;
         }
     }
 
-    // E-mail content
-    $onderwerp = "Nieuwe uren ingediend door " . $user_name;
-    $boodschap = "De volgende uren zijn ingediend door " . $user_name . " voor week " . $weeknummer . ":\n\n" . $uren_leesbaar;
-    $boodschap .= "\n\nTotaal aantal uren: " . $totaal_uren . " uur";
-    $boodschap .= "\n\nLog nu in om de uren te beoordelen: " . wp_login_url();
-    $headers = array('Content-Type: text/html; charset=UTF-8');
+    $subject = 'Nieuwe uren ingediend door ' . $user_name;
 
-    // Verzend de e-mail
-    wp_mail($notification_email, $onderwerp, nl2br($boodschap), $headers);
+    $message = 'Beste ' . $opdrachtgever_info->display_name . ",\n\n";
+    $message .= 'Er zijn nieuwe uren ingediend door ' . $user_name . " voor week " . $weeknummer . ":\n\n";
+    $message .= $uren_leesbaar;
+    $message .= "\n\nTotaal aantal uren: " . $totaal_uren . " uur";
+    $message .= "\n\nBekijk de ingediende uren in het dashboard: " . site_url('/wp-admin') . "\n\n";
+    $message .= 'Met vriendelijke groet,\n';
+    $message .= 'Het Uren Registratie Team';
+
+    wp_mail($opdrachtgever_info->user_email, $subject, nl2br($message), array('Content-Type: text/html; charset=UTF-8'));
+
+    wp_mail($admin_email, $subject, nl2br($message), array('Content-Type: text/html; charset=UTF-8'));
 }
 
-// Trigger wanneer uren worden ingediend (auto verzenden)
-add_action('publish_uren', 'urenregistratie_verzend_email');
+add_action('save_post_uren', 'send_hours_submission_email');
